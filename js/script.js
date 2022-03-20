63e9d042548273e854d98ebdb69889035cb4fb24
Andrey <kadlubenko@gmail.com>
2022-03-20 15:32:00 +0200

    secondQvest

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-alert('Ok!');
+const  numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+const personalMovieDB = {
+count: numberOfFilms,
+movies:{},
+actors:{},
+genres:[],
+privat: false
+};
+let i = 1;
+while(i<=2){
+let filmName = prompt('Один из последних просмотренных фильмов?');
+let pris = prompt('Насколько оцените его?');
+personalMovieDB.movies[filmName] = pris;
+i++;
+}
+console.log(personalMovieDB);
